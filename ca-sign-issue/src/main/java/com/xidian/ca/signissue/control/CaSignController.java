eafad320a5980ed99289732ec1d5c72e897ac34b
lvliuwei <lvliuwei@netposa.com>
2017-08-15 16:16:34 +0800

    [新增]增加CA机构的管理模块[ca-management]和签发[ca-sign-issue]模块

---
+++ ca-sign-issue/src/main/java/com/xidian/ca/signissue/control/CaSignController.java
@@ -0,0 +1,11 @@
+package com.xidian.ca.signissue.control;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by LvLiuWei on 2017/8/15.
+ */
+@RestController
+public class CaSignController {
+
+}
